bf31167aa9ec4a5ff79bd9f504090505004f02cd
Waleed Lugod <lugodwaleed@gmail.com>
2026-03-27 15:23:07 +0800

    evaluate

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 # ALPHABET: !<>()[]{}x
 
 
-def is_balanced(input):
+def is_balanced(input, write_to_output):
     output = open("output.txt", "a")
-    output.write(f"Processing {input}\n")
+    if write_to_output:
+        output.write(f"Processing {input}\n")
 
     state = "q0"
     stack = ["Z"]
     error = False
 
     input += "E"
+    # change to while true and more verbose
     for i in range(len(input)):
-        if i == len(input) - 1:
-            output.write(
-                f"ID: ({state}, {input[i:]}, {''.join(list(reversed(stack)))})\n"
-            )
-        else:
-            output.write(
-                f"ID: ({state}, {input[i:-1]}, {''.join(list(reversed(stack)))})\n"
-            )
+        if write_to_output:
+            if i == len(input) - 1:
+                output.write(
+                    f"ID: ({state}, {input[i:]}, {''.join(list(reversed(stack)))})\n"
+                )
+            else:
+                output.write(
+                    f"ID: ({state}, {input[i:-1]}, {''.join(list(reversed(stack)))})\n"
+                )
 
         symbol = input[i]
+
         if state == "q0":
             if symbol == "!" and stack[-1] == "Z":
                 stack.append("!")
@@ -47,35 +51,72 @@ def is_balanced(input):
                 error = True
 
         elif state == "q2" and symbol == "E":
-            output.write("q2 is a final state.\n")
-            output.write(f"{input[:-1]} is valid and has balanced brackets.\n\n")
+            if write_to_output:
+                output.write("q2 is a final state.\n")
+                output.write(f"{input[:-1]} is valid and has balanced brackets.\n\n")
             return True
 
         if error:
-            output.write(f"Invalid string. Failed at position {i+1}.\n")
-            output.write(f"Remaining unprocessed input string: {input[i:-1]}\n\n")
+            if write_to_output:
+                output.write(f"Invalid string. Failed at position {i+1}.\n")
+                output.write(f"Remaining unprocessed input string: {input[i:-1]}\n\n")
             return False
-    output.write(f"Invalid string. {state} is not a final state.\n\n")
+    if write_to_output:
+        output.write(f"Invalid string. {state} is not a final state.\n\n")
     return False
 
 
 def evaluate(input):
-    pass
+    stack = []
+    i = 0
+    while True:
+        new_string = input
+        c = input[i]
+        if c == "!" and i > 0:
+            break
+        elif c == "<" or c == "(" or c == "[" or c == "{":
+            stack.append([c, i])
+            i += 1
+        elif c == ">" and stack[-1][0] == "<":
+            top = stack.pop()
+            content = input[top[1] + 1 : i]
+            new_string = input[: top[1]] + content * 2 + input[i + 1 :]
+            i += len(content) - 2
+        elif c == ")" and stack[-1][0] == "(":
+            top = stack.pop()
+            content = input[top[1] + 2 : i]
+            new_string = input[: top[1]] + content + input[i + 1 :]
+            i -= 1 + len(content)
+        elif c == "]" and stack[-1][0] == "[":
+            top = stack.pop()
+            content = input[top[1] + 1 : i]
+            new_string = input[: top[1]] + input[i + 1 :]
+            i -= len(content) + 1
+        elif c == "}" and stack[-1][0] == "{":
+            top = stack.pop()
+            content = input[top[1] + 1 : i] + "x"
+            new_string = input[: top[1]] + content + input[i + 1 :]
+        else:
+            i += 1
+        input = new_string
+        print(input)
+    return len(input) - 2
 
 
 def main1():
     input = open("input.txt")
     for i in input:
-        is_balanced(i.strip())
+        is_balanced(i.strip(), True)
 
 
 def main2():
     input = open("input.txt")
     for i in input:
-        if is_balanced(i):
-            evaluate(i)
+        if is_balanced(i.strip(), False):
+            evaluate(i.strip())
 
 
 if __name__ == "__main__":
     open("output.txt", "w").close()
     main1()
+    main2()
