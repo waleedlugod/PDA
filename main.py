1c8c46c5e81015f9be1f695173448781edf38c75
Waleed Lugod <lugodwaleed@gmail.com>
2026-03-27 16:20:34 +0800

    refactor is_balanced

--- main.py
+++ main.py
@@ -1,69 +1,180 @@
-# ALPHABET: !<>()[]{}x
+# def is_balanced(input, write_to_output):
+#     output = open("output.txt", "a")
+#     if write_to_output:
+#         output.write(f"Processing {input}\n")
+
+#     state = "q0"
+#     stack = ["Z"]
+#     error = False
+
+#     input += "E"
+#     # change to while true and more verbose
+#     for i in range(len(input)):
+#         if write_to_output:
+#             if i == len(input) - 1:
+#                 output.write(
+#                     f"ID: ({state}, {input[i:]}, {''.join(list(reversed(stack)))})\n"
+#                 )
+#             else:
+#                 output.write(
+#                     f"ID: ({state}, {input[i:-1]}, {''.join(list(reversed(stack)))})\n"
+#                 )
+
+#         symbol = input[i]
+
+#         if state == "q0":
+#             if symbol == "!" and stack[-1] == "Z":
+#                 stack.append("!")
+#                 state = "q1"
+#             else:
+#                 error = True
+
+#         elif state == "q1":
+#             if symbol == "x" or symbol == "E":
+#                 pass
+#             elif symbol == "<" or symbol == "(" or symbol == "[" or symbol == "{":
+#                 stack.append(symbol)
+#             elif (
+#                 (symbol == ">" and stack[-1] == "<")
+#                 or (symbol == ")" and stack[-1] == "(")
+#                 or (symbol == "]" and stack[-1] == "[")
+#                 or (symbol == "}" and stack[-1] == "{")
+#             ):
+#                 stack.pop()
+#             elif symbol == "!" and stack[-1] == "!":
+#                 stack.pop()
+#                 state = "q2"
+#             else:
+#                 error = True
+
+#         elif state == "q2" and symbol == "E":
+#             if write_to_output:
+#                 output.write("q2 is a final state.\n")
+#                 output.write(f"{input[:-1]} is valid and has balanced brackets.\n\n")
+#             return True
+
+#         if error:
+#             if write_to_output:
+#                 output.write(f"Invalid string. Failed at position {i+1}.\n")
+#                 output.write(f"Remaining unprocessed input string: {input[i:-1]}\n\n")
+#             return False
+#     if write_to_output:
+#         output.write(f"Invalid string. {state} is not a final state.\n\n")
+#     return False
 
 
-def is_balanced(input, write_to_output):
+def is_balanced(input, is_write_to_output):
     output = open("output.txt", "a")
-    if write_to_output:
+
+    states = ("q0", "q1", "q2")
+    alphabet = "!<>()[]{}x"
+    transitions = {
+        # !
+        ("q0", "!", "Z"): ["q1", "Z", "!"],
+        ("q1", "!", "!"): ["q2"],
+        # <
+        ("q1", "<", "!"): ["q1", "!", "<"],
+        ("q1", "<", "<"): ["q1", "<", "<"],
+        ("q1", "<", ">"): ["q1", ">", "<"],
+        ("q1", "<", "("): ["q1", "(", "<"],
+        ("q1", "<", ")"): ["q1", ")", "<"],
+        ("q1", "<", "["): ["q1", "[", "<"],
+        ("q1", "<", "]"): ["q1", "]", "<"],
+        ("q1", "<", "{"): ["q1", "{", "<"],
+        ("q1", "<", "}"): ["q1", "}", "<"],
+        # (
+        ("q1", "(", "!"): ["q1", "!", "("],
+        ("q1", "(", "<"): ["q1", "<", "("],
+        ("q1", "(", ">"): ["q1", ">", "("],
+        ("q1", "(", "("): ["q1", "(", "("],
+        ("q1", "(", ")"): ["q1", ")", "("],
+        ("q1", "(", "["): ["q1", "[", "("],
+        ("q1", "(", "]"): ["q1", "]", "("],
+        ("q1", "(", "{"): ["q1", "{", "("],
+        ("q1", "(", "}"): ["q1", "}", "("],
+        # [
+        ("q1", "[", "!"): ["q1", "!", "["],
+        ("q1", "[", "<"): ["q1", "<", "["],
+        ("q1", "[", ">"): ["q1", ">", "["],
+        ("q1", "[", "("): ["q1", "(", "["],
+        ("q1", "[", ")"): ["q1", ")", "["],
+        ("q1", "[", "["): ["q1", "[", "["],
+        ("q1", "[", "]"): ["q1", "]", "["],
+        ("q1", "[", "{"): ["q1", "{", "["],
+        ("q1", "[", "}"): ["q1", "}", "["],
+        # {
+        ("q1", "{", "!"): ["q1", "!", "{"],
+        ("q1", "{", "<"): ["q1", "<", "{"],
+        ("q1", "{", ">"): ["q1", ">", "{"],
+        ("q1", "{", "("): ["q1", "(", "{"],
+        ("q1", "{", ")"): ["q1", ")", "{"],
+        ("q1", "{", "["): ["q1", "[", "{"],
+        ("q1", "{", "]"): ["q1", "]", "{"],
+        ("q1", "{", "{"): ["q1", "{", "{"],
+        ("q1", "{", "}"): ["q1", "}", "{"],
+        # >)]}
+        ("q1", ">", "<"): ["q1"],
+        ("q1", ")", "("): ["q1"],
+        ("q1", "]", "["): ["q1"],
+        ("q1", "}", "{"): ["q1"],
+        # x
+        ("q1", "x", "!"): ["q1", "!"],
+        ("q1", "x", "<"): ["q1", "<"],
+        ("q1", "x", ">"): ["q1", ">"],
+        ("q1", "x", "("): ["q1", "("],
+        ("q1", "x", ")"): ["q1", ")"],
+        ("q1", "x", "["): ["q1", "["],
+        ("q1", "x", "]"): ["q1", "]"],
+        ("q1", "x", "{"): ["q1", "{"],
+        ("q1", "x", "}"): ["q1", "}"],
+    }
+    finals = "q2"
+
+    if is_write_to_output:
         output.write(f"Processing {input}\n")
 
-    state = "q0"
-    stack = ["Z"]
-    error = False
-
     input += "E"
-    # change to while true and more verbose
-    for i in range(len(input)):
-        if write_to_output:
+
+    stack = ["Z"]
+    curr_state = "q0"
+    i = 0
+    while True:
+        if is_write_to_output:
             if i == len(input) - 1:
                 output.write(
-                    f"ID: ({state}, {input[i:]}, {''.join(list(reversed(stack)))})\n"
+                    f"ID: ({curr_state}, {input[i:]}, {''.join(list(reversed(stack)))})\n"
                 )
             else:
                 output.write(
-                    f"ID: ({state}, {input[i:-1]}, {''.join(list(reversed(stack)))})\n"
+                    f"ID: ({curr_state}, {input[i:-1]}, {''.join(list(reversed(stack)))})\n"
                 )
-
         symbol = input[i]
+        top = stack.pop()
 
-        if state == "q0":
-            if symbol == "!" and stack[-1] == "Z":
-                stack.append("!")
-                state = "q1"
-            else:
-                error = True
-
-        elif state == "q1":
-            if symbol == "x" or symbol == "E":
-                pass
-            elif symbol == "<" or symbol == "(" or symbol == "[" or symbol == "{":
-                stack.append(symbol)
-            elif (
-                (symbol == ">" and stack[-1] == "<")
-                or (symbol == ")" and stack[-1] == "(")
-                or (symbol == "]" and stack[-1] == "[")
-                or (symbol == "}" and stack[-1] == "{")
-            ):
-                stack.pop()
-            elif symbol == "!" and stack[-1] == "!":
-                stack.pop()
-                state = "q2"
-            else:
-                error = True
-
-        elif state == "q2" and symbol == "E":
-            if write_to_output:
+        key = (curr_state, symbol, top)
+        if curr_state == finals:
+            if is_write_to_output:
                 output.write("q2 is a final state.\n")
                 output.write(f"{input[:-1]} is valid and has balanced brackets.\n\n")
             return True
-
-        if error:
-            if write_to_output:
-                output.write(f"Invalid string. Failed at position {i+1}.\n")
-                output.write(f"Remaining unprocessed input string: {input[i:-1]}\n\n")
+        elif key in transitions:
+            transition = transitions[key]
+            curr_state = transition[0]
+            stack.extend(transition[1:])
+        else:
+            if is_write_to_output:
+                if symbol == "E":
+                    output.write(
+                        f"Invalid string. {curr_state} is not a final state.\n\n"
+                    )
+                else:
+                    output.write(f"Invalid string. Failed at position {i+1}.\n")
+                    output.write(
+                        f"Remaining unprocessed input string: {input[i:-1]}\n\n"
+                    )
             return False
-    if write_to_output:
-        output.write(f"Invalid string. {state} is not a final state.\n\n")
-    return False
+
+        i += 1
 
 
 def evaluate(input):
